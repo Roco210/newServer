1e435a5529413f8ca16a0585f7f50fa6c82543fa
RicardoF <rickyfernandez210@gmail.com>
2023-10-05 22:09:58 -0300

    fixbug

--- src/services/user.services.js
+++ src/services/user.services.js
@@ -1,29 +1,22 @@
 import {userMongo} from "../DAL/manager/users/usersManagerMongo.js"
 
 export const checkData =(req, res,next) => {
-    const { firstName, lastName, email, age, password } = req.body
-    console.log(req.body)
-    if (!firstName|| !lastName || !email || !age || !password ) {
+    const { first_name,last_name, email, age, password } = req.body
+    if (!first_name|| !last_name || !email || !age || !password ) {
         res.status(400).json({ messge: "faltan datos" })
         return
     }
-    req.user = req.body.first_name.first_name
+    const user = req.body
+    req.user = user
     next()
 }
 
-export const userLog =async (req, res,next) => {
-    const {email,password}=req.body
-    if (!email || !password) {
-        res.status(400).json({ messge: "faltan datos" })}
-    const userDb = await userMongo.findUser(email)
-    req.user=userDb
-    next()
-}
 
 
 export const userInfo= (user)=>{
+
     const objUser=[{
-        first_name:user.first_name.toUpperCase() ,
+        first_name:user.first_name?user.first_name:"null",
         rol:user.isAdmin ? "ADMIN":"USER"
     }]
     return objUser
